96283f2199fef0a6ad58a2c44cfb115ee432b6ee
Xavi <javier19pedraza@gmail.com>
2021-03-24 09:19:55 -0500

    template and show.blade was created and organized

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- title --}}
+    <title>@yield('title')</title>
+    {{-- bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    {{-- favicon --}}
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+
+</head>
+<body>
+    {{-- header --}}
+    {{-- nav --}}
+    {{-- content --}}
+
+@yield('content')
+
+{{-- footer --}}
+{{-- script --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/show.blade.php
@@ -0,0 +1,6 @@
+
+@extends('layouts.template')
+@section('title')
+@section('content')
+
+@endsection
